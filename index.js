7a6e9b4e7d02d9e6f948ac8d0cc1cb12e948011e
Naimaabdullahi <naima.noor@student.moringaschool.com>
2025-03-21 15:32:50 +0300

    DOM

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(e) {
+    const content =document.querySelector('#text')
+    content.textContent = 'This is really cool!'
+})
